422fc61fe1bb6e9dc30d7b27052c3d02f380b35f
yskim6772 <yskim6772@gmail.com>
2024-11-18 03:06:02 +0900

    #1 [setting] : font system setting

--- app/src/main/java/com/example/market_kurly/ui/theme/Theme.kt
+++ app/src/main/java/com/example/market_kurly/ui/theme/Theme.kt
@@ -1,37 +1,40 @@
 package com.example.market_kurly.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryColor600
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryColor600
 )
+
+private val LocalMarketKurlyTypography = staticCompositionLocalOf<MarketKurlyTypography> {
+    error("No MarketKurlyTypography provided")
+}
+
+object MarketKurlyTheme {
+    val typography: MarketKurlyTypography
+        @Composable get() = LocalMarketKurlyTypography.current
+}
+
+@Composable
+fun ProvideMarketKurlyTypography(typography: MarketKurlyTypography, content: @Composable () -> Unit) {
+    val provideTypography = remember { typography.copy() }
+    provideTypography.update(typography)
+    CompositionLocalProvider(
+        LocalMarketKurlyTypography provides provideTypography,
+        content = content
+    )
+}
 
 @Composable
 fun MARKETKURLYTheme(
@@ -40,19 +43,13 @@ fun MARKETKURLYTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = LightColorScheme
+    val typography = marketKurlyTypography()
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    ProvideMarketKurlyTypography(typography) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content,
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
--- app/src/main/java/com/example/market_kurly/ui/theme/Type.kt
+++ app/src/main/java/com/example/market_kurly/ui/theme/Type.kt
@@ -1,34 +1,118 @@
 package com.example.market_kurly.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.example.market_kurly.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+val NotoSansKrExtraBold = FontFamily(Font(R.font.noto_sans_kr_extrabold))
+val NotoSansKrBold = FontFamily(Font(R.font.noto_sans_kr_bold))
+val NotoSansKrMedium = FontFamily(Font(R.font.noto_sans_kr_medium))
+val NotoSansKrNormal = FontFamily(Font(R.font.noto_sans_kr_regular))
+
+data class MarketKurlyTypography(
+    val titleEmoji22: TextStyle = TextStyle(
+        fontFamily = NotoSansKrExtraBold,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
+        lineHeight = 22.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    val titleB22: TextStyle = TextStyle(
+        fontFamily = NotoSansKrBold,
+        fontSize = 22.sp,
+        lineHeight = 26.sp,
+        letterSpacing = 0.sp
+    ),
+    val titleB18: TextStyle = TextStyle(
+        fontFamily = NotoSansKrBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (-3 / 18).sp
+    ),
+    val titleM18: TextStyle = TextStyle(
+        fontFamily = NotoSansKrMedium,
+        fontSize = 18.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.sp
+    ),
+    val bodyB16: TextStyle = TextStyle(
+        fontFamily = NotoSansKrBold,
+        fontSize = 16.sp,
+        lineHeight = 23.sp,
+        letterSpacing = (-2 / 16).sp
+    ),
+    val bodyM16: TextStyle = TextStyle(
+        fontFamily = NotoSansKrMedium,
+        fontSize = 16.sp,
+        lineHeight = 23.sp,
+        letterSpacing = (-3 / 16).sp
+    ),
+    val bodyR16: TextStyle = TextStyle(
+        fontFamily = NotoSansKrNormal,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = (-1 / 16).sp
+    ),
+    val bodyR15: TextStyle = TextStyle(
+        fontFamily = NotoSansKrNormal,
+        fontSize = 15.sp,
+        lineHeight = 19.sp,
+        letterSpacing = (-3 / 15).sp
+    ),
+    val bodyB14: TextStyle = TextStyle(
+        fontFamily = NotoSansKrBold,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        letterSpacing = (-6 / 14).sp
+    ),
+    val bodyM14: TextStyle = TextStyle(
+        fontFamily = NotoSansKrMedium,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        letterSpacing = (-7 / 14).sp
+    ),
+    val bodyR14: TextStyle = TextStyle(
+        fontFamily = NotoSansKrNormal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    val captionB12: TextStyle = TextStyle(
+        fontFamily = NotoSansKrBold,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = (-4 / 12).sp
+    ),
+    val captionM12: TextStyle = TextStyle(
+        fontFamily = NotoSansKrMedium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = (-1 / 12).sp
+    ),
+    val captionR12: TextStyle = TextStyle(
+        fontFamily = NotoSansKrNormal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = (-1 / 12).sp
+    ),
+    val captionM11: TextStyle = TextStyle(
+        fontFamily = NotoSansKrMedium,
+        fontSize = 11.sp,
+        lineHeight = 15.sp,
+        letterSpacing = (-4 / 11).sp
+    ),
+    val captionM9: TextStyle = TextStyle(
+        fontFamily = NotoSansKrMedium,
+        fontSize = 9.sp,
+        lineHeight = 12.sp,
+        letterSpacing = (-1 / 9).sp
     )
-    */
-)
+) {
+    fun copy(): MarketKurlyTypography = this
+
+    fun update(other: MarketKurlyTypography) {}
+}
+
+fun marketKurlyTypography(): MarketKurlyTypography = MarketKurlyTypography()
