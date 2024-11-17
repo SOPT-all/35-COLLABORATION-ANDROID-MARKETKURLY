803182c068ae6e64cabe348b0d53284548cec279
yskim6772 <yskim6772@gmail.com>
2024-11-18 03:06:10 +0900

    #1 [setting] : color system setting

--- app/src/main/java/com/example/market_kurly/ui/theme/Color.kt
+++ app/src/main/java/com/example/market_kurly/ui/theme/Color.kt
@@ -2,10 +2,38 @@ package com.example.market_kurly.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary Color
+val PrimaryColor200 = Color(0xFFFAF4FE)
+val PrimaryColor300 = Color(0xFFDDB7FE)
+val PrimaryColor400 = Color(0xFFBD76FF)
+val PrimaryColor500 = Color(0xFF6F198C)
+val PrimaryColor600 = Color(0xFF5F0080)
+val PrimaryColor700 = Color(0xFF351358)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Color
+val Red = Color(0xFFFA622F)
+val Mint1 = Color(0xFFF3FBFD)
+val Mint2 = Color(0xFFB6DEE8)
+val Mint3 = Color(0xFF4DBED7)
+
+// Gray Scale
+val White = Color(0xFFFFFFFF)
+val Gray1 = Color(0xFFFCFCFC)
+val Gray2 = Color(0xFFF5F5F5)
+val Gray3 = Color(0xFFDDDDDD)
+val Gray4 = Color(0xFFB6B6B6)
+val Gray5 = Color(0xFF999999)
+val Gray6 = Color(0xFF666666)
+val Gray7 = Color(0xFF333333)
+val Gray8 = Color(0xFF222222)
+val CoolGray1 = Color(0xFFF2F5F8)
+val CoolGray2 = Color(0xFFDEE4EC)
+val CoolGray3 = Color(0xFFBAC4CD)
+val CoolGray4 = Color(0xFF818F9B)
+val CoolGray5 = Color(0xFF545E68)
+
+// Transparent
+val Black18 = Color(0x2E000000)
+
+// Gradient
+val GrGray1 = Color(0xFFEBEEF3)
