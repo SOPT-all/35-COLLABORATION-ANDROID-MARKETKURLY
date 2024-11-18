78e0e6bf1af38c192599ff770a1ea549c062144a
yskim6772 <yskim6772@gmail.com>
2024-11-18 09:33:37 +0900

    #1 [setting] : add KeyStorage, delete unnecessary import

---
+++ app/src/main/java/com/example/market_kurly/core/util/KeyStorage.kt
@@ -0,0 +1,7 @@
+package com.example.market_kurly.core.util
+
+object KeyStorage {
+    // 상수화 예시
+    const val API = "api"
+    const val V1 = "v1"
+}
--- app/src/main/java/com/example/market_kurly/ui/theme/Type.kt
+++ app/src/main/java/com/example/market_kurly/ui/theme/Type.kt
@@ -3,7 +3,6 @@ package com.example.market_kurly.ui.theme
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.market_kurly.R
 
