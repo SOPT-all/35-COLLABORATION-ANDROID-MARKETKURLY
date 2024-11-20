4dce1259807efd75e408edcea4d62f5cb9741d06
serioushyeon <zz64446949@gmail.com>
2024-11-20 17:54:16 +0900

    #3 [del] : 불필요 라이브러리 임포트 삭제

--- app/src/main/java/com/example/market_kurly/core/dummymodel/AlsoViewedData.kt
+++ app/src/main/java/com/example/market_kurly/core/dummymodel/AlsoViewedData.kt
@@ -1,7 +1,5 @@
 package com.example.market_kurly.core.dummymodel
 
-import androidx.annotation.DrawableRes
-
 data class AlsoViewedData(
     val image : String,
     val goodsName: String,
--- app/src/main/java/com/example/market_kurly/feature/goods/GoodsScreen.kt
+++ app/src/main/java/com/example/market_kurly/feature/goods/GoodsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.market_kurly.feature.goods
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
