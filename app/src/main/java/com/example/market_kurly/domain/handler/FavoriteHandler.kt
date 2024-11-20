f9320a70f4df158ed6f66ac7da80139328c8b087
serioushyeon <zz64446949@gmail.com>
2024-11-20 19:47:46 +0900

    #3 [del] : 불필요 파일 삭제

--- app/src/main/java/com/example/market_kurly/domain/handler/FavoriteHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.example.market_kurly.domain.handler
-
-import kotlinx.coroutines.flow.StateFlow
-
-interface FavoriteHandler {
-    val isFavorite: StateFlow<Boolean>
-    fun toggleFavorite()
-}
