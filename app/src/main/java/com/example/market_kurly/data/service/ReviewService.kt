e8a30623b58c0d924477cef0e26abb7472d3df25
yskim6772 <yskim6772@gmail.com>
2024-11-27 02:14:04 +0900

    #11 [feat] : ReviewService, dto

---
+++ app/src/main/java/com/example/market_kurly/data/service/ReviewService.kt
@@ -0,0 +1,21 @@
+package com.example.market_kurly.data.service
+
+import com.example.market_kurly.data.dto.base.BaseResponse
+import com.example.market_kurly.data.dto.response.ResponseReviewsDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ReviewService {
+
+    companion object {
+        const val API = "api"
+        const val V1 = "v1"
+        const val PRODUCTS = "products"
+        const val REVIEWS = "reviews"
+    }
+
+    @GET("/$API/$V1/$PRODUCTS/{productId}/$REVIEWS")
+    suspend fun getProductReviews(
+        @Path("productId") productId : Number
+    ) : BaseResponse<ResponseReviewsDto>
+}
